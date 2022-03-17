6f9bb06e5550dde4d3cfe9abaffa0449cc2cac06
Mustafa <mustafanck3@gmail.com>
2022-03-17 20:06:59 +0300

    User models added

--- LifeFitsHome/Contexts/DbContextBase.cs
+++ LifeFitsHome/Contexts/DbContextBase.cs
@@ -5,7 +5,37 @@ namespace LifeFitsHome.Contexts
 {
     public class DbContextBase : DbContext
     {
-        public DbSet<User> Users { get; set; }
-        public DbSet<Claim> Claims { get; set; }
+        public DbSet<User>? Users { get; set; }
+        public DbSet<OperationClaim>? OperationClaims { get; set; }
+        public DbSet<UserOperationClaim>? UserOperationClaims { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var serverVersion = new MySqlServerVersion(new Version(8, 0, 28));
+            optionsBuilder.UseMySql("server=localhost;database=lifefitshomedb;user=root;port=3306;password=toortoor", serverVersion);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Email).IsRequired();
+                entity.Property(e => e.IdentyNumber).IsRequired();
+                entity.Property(e => e.PhoneNumber).IsRequired();
+                entity.Property(e => e.PasswordSalt);
+                entity.Property(e => e.PasswordHash);
+                entity.Property(e => e.FirstName).IsRequired();
+                entity.Property(e => e.LastName).IsRequired();
+                entity.Property(e => e.IsBlocked).IsRequired();
+                entity.Property(e => e.IsSafety).IsRequired();
+            });
+            modelBuilder.Entity<OperationClaim>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Name).IsRequired();
+            });
+        }
     }
+
 }
