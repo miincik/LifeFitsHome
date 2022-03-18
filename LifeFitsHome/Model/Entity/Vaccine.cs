f5672f834cb225e64e206cb0914fd33947202286
pirematur3 <ozkean.93@gmail.com>
2022-03-18 14:24:01 +0300

    vaccines contex created

--- LifeFitsHome/Model/Entity/Vaccine.cs
+++ LifeFitsHome/Model/Entity/Vaccine.cs
@@ -7,5 +7,6 @@ namespace LifeFitsHome.Model.Entity
         public string? Description { get; set; }
         public VaccineType? VaccineType { get; set; }
         public int VaccineTypeId { get; set; }
+        public virtual ICollection<User>? Users { get; set; }
     }
 }
--- LifeFitsHome/Model/Entity/VaccineType.cs
+++ LifeFitsHome/Model/Entity/VaccineType.cs
@@ -1,6 +1,9 @@
-namespace LifeFitsHome.Model.Entity{
-    public class VaccineType : IEntity{
+namespace LifeFitsHome.Model.Entity
+{
+    public class VaccineType : IEntity
+    {
         public int Id { get; set; }
         public string? Name { get; set; }
+        public virtual ICollection<Vaccine>? Vaccines { get; set; }
     }
 }
